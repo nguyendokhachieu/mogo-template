fdbf8c96dc368883633efe7539a20620d174c711
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-04-22 02:37:48 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
-const headerNav = document.getElementById("header-nav");
-const toggleButton = document.getElementById("menu-toggle");
+$(document).ready(function () {
+  const headerNav = document.getElementById("header-nav");
+  const toggleButton = document.getElementById("menu-toggle");
 
-var isToggled = true;
-toggleButton.addEventListener("click", () => {
-    if (isToggled) {
+  $(".quote-list").slick();
+
+  $("button.slick-arrow.slick-prev")
+    .addClass("fa fa-arrow-left arrow-icon")
+    .text("");
+  $("button.slick-arrow.slick-next")
+    .addClass("fa fa-arrow-right arrow-icon")
+    .text("");
+
+  var isMenuToggled = false;
+  window.addEventListener("click", (e) => {
+    const menuClasses = e.target.classList.value;
+    if (menuClasses.indexOf("menu-toggle") != -1) {
+      if (isMenuToggled == false) {
         headerNav.classList.add("is-toggled");
-        isToggled = false;
-    } else {
+        toggleButton.classList.add("outline-dash");
+        isMenuToggled = true;
+      } else {
         headerNav.classList.remove("is-toggled");
-        isToggled = true;
+        toggleButton.classList.remove("outline-dash");
+        isMenuToggled = false;
+      }
+    } else if (
+      menuClasses.indexOf("header-nav-item") == -1 &&
+      menuClasses.indexOf("header-nav-link") == -1
+    ) {
+      if (isMenuToggled == true) {
+        headerNav.classList.remove("is-toggled");
+        toggleButton.classList.remove("outline-dash");
+        isMenuToggled = false;
+      }
     }
+  });
 });
